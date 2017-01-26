42f1d4dfcf50e68666dcc9fed25f56487c3b05f7
JD Sareault <jdsareault@gmail.com>
2017-01-26 00:07:45 -0500

    added rot13 crypto functionality to webform

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import webapp2
-import string
+import cgi
 from crypto import encrypt
 
 form = """<h1>Enter some text to ROT13:</h1>
-<form method="post" action="/submit">
-    <textarea name="text" rows="6" cols="40">Test</textarea><br><br>
+<form method="post" action="/">
+    <textarea name="text" rows="6" cols="40">%(message)s</textarea><br><br>
     <input type="submit">
 </form>"""
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write(form)
+    def write_form(self, message=""):
+        self.response.write(form % {"message": message})
 
+    def get(self):
+        self.write_form()
+
+    def post(self):
+        encrypted_message = encrypt(self.request.get("text"),13)
+        sanitized_message = cgi.escape(encrypted_message, True)
+        self.write_form(sanitized_message)
 
 
 app = webapp2.WSGIApplication([
